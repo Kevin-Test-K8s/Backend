0df1b043d722e1d3ca7f91da9bd84683eba5845c
kevden01 <kevinklein@mac.com>
2025-04-05 14:20:19 +0200

    testing versioning

---
+++ version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VERSION_FILE="VERSION"
+
+# Read current version
+CURRENT_VERSION=$(cat "$VERSION_FILE")
+
+# Export to GitHub Actions
+echo "VERSION=$CURRENT_VERSION" >> $GITHUB_ENV
+
+# Increment version for next build
+MAJOR=$(echo "$CURRENT_VERSION" | cut -d. -f1)
+MINOR=$(echo "$CURRENT_VERSION" | cut -d. -f2)
+NEXT_MINOR=$((MINOR + 1))
+NEXT_VERSION="$MAJOR.$NEXT_MINOR"
+
+# Save next version to file
+echo "$NEXT_VERSION" > "$VERSION_FILE"
